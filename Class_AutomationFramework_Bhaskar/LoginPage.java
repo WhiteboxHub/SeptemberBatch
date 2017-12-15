5cc33433aacdb6e1307dd8ae5a0da57f28ad3952
WBL-Training <training@whitebox-learning.com>
2017-12-15 10:19:04 -0800

    Create LoginPage.java

---
+++ Class_AutomationFramework_Bhaskar/LoginPage.java
@@ -0,0 +1,13 @@
+package com.wbl.page;
+
+import org.openqa.selenium.WebDriver;
+
+import com.wbl.base.CommonPage;
+
+public class LoginPage extends CommonPage {
+
+	public LoginPage(WebDriver driver) {
+		super(driver);
+	}
+
+}
